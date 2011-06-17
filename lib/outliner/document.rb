3eb4e48cb665c0a26556d83681210028b85700a7
brendanhay <brendan.g.hay@gmail.com>
2011-06-17 14:42:26 +1200

    Moving outlinee related predicates out of document and onto the outlinee instance

--- lib/outliner/document.rb
+++ lib/outliner/document.rb
@@ -66,13 +66,12 @@ MARKDOWN
     end
 
     def enter_node(outlinee)
+      # TODO: Investigate why this breaks the algorithm (without too much handwaving)
       # If the top of the stack is a heading content element - do nothing
-      if @stack.any? && heading?(@stack.last)
-        return
-      end
+      # return if @stack.any? && heading?(@stack.last)
       
       # When entering a sectioning content element or a sectioning root element
-      if any_section? outlinee
+      if outlinee.content_or_root?
         # If current outlinee is not null, push current outlinee onto the stack
         @stack.push(@outlinee) if @outlinee
      
@@ -92,12 +91,12 @@ MARKDOWN
       return unless @outlinee
 
       # When entering a heading content element
-      if heading? outlinee
+      if outlinee.heading? 
         # If the current section has no heading, let the element being entered be the heading for the current section
         if !@section.heading?
           @section.heading = outlinee 
         # Otherwise, if the element being entered has a rank equal to or greater than the heading of the last section of the outline of the current outlinee,
-        elsif rank(outlinee) >= rank(@outlinee.outlinee.last_section.heading)
+        elsif outlinee.rank >= @outlinee.outline.last_section.heading.rank
           # Create a new section
           section = Section.new
 
@@ -118,7 +117,7 @@ MARKDOWN
 
           while !abort
             # 2. If the element being entered has a rank lower than the rank of the heading of the candidate section
-            if rank(outlinee) < rank(candidate.heading)
+            if outlinee.rank < candidate.heading.rank
               # Create a new section
               section = Section.new
 
@@ -151,22 +150,17 @@ MARKDOWN
       # Note: The element being exited is a heading content element.
       # Pop that element from the stack.
       # If the top of the stack is a heading content element - do nothing
-      if @stack.any? && heading?(@stack.last)
+      if @stack.any? && @stack.last.heading?
         @stack.pop if @stack.last == outlinee
-
         return
       end
 
-      # ************ MODIFICATION OF ORIGINAL ALGORITHM *****************
-      # existing sectioning content or sectioning root
-      # this means, currentSection will change (and we won't get back to it)
-      if any_section?(outlinee) && !@section.heading?
-        @section.heading = Nokogiri::XML::Node.new 'i', @html
-      end
-      # ************ END MODIFICATION ***********************************
+#       if outlinee.content_or_root? && !@section.heading?
+#         @section.heading = Nokogiri::XML::Node.new 'i', @html
+#       end
 
       # When exiting a sectioning content element, if the stack is not empty
-      if content_section?(outlinee) && @stack.any?
+      if outlinee.content? && @stack.any?
         # Pop the top element from the stack, and let the current outlinee be that element
         @outlinee = @stack.pop
 
@@ -183,7 +177,7 @@ MARKDOWN
       end
 
       # When exiting a sectioning root element, if the stack is not empty
-      if root_section?(outlinee) && @stack.any?
+      if outlinee.root? && @stack.any?
         # Pop the top element from the stack, and let the current outlinee be that element
         @outlinee = @stack.pop
 
@@ -199,114 +193,9 @@ MARKDOWN
       end
 
       # When exiting a sectioning content element or a sectioning root element
-      if any_section?(outlinee)
+      if outlinee.content_or_root?
         @section = @outlinee.outline.first_child
       end # Skip to the next step in the overall set of steps. (The walk is over.)
-    end
-
-    #   puts "entering: #{outlinee.node.name}"
-
-    #   if any_section? outlinee # 4c
-    #     puts "4c"
-    #     @stack.push @outlinee unless @outlinee.nil? # 4c.2
-    #     @outlinee = outlinee # 4c.3
-    #     @section = Section.new # 4c.4, 4c.5
-    #     @section.outlinee = @outlinee      
-    #     @outlinee.outline = Outline.new @section # 4c.6
-    #     return
-    #   end
-
-    #   return if @outlinee.nil?
-
-    #   if heading? outlinee # 4h
-    #     puts "4h"
-    #     if !@section.heading? # 4h.1
-    #       @section.heading = outlinee 
-    #     elsif rank(outlinee) >= rank(@outlinee.outline.last_section.heading) # 4h.2
-    #       puts "4h.2"
-    #       puts @outlinee.node.name
-    #       @section = Section.new 
-    #       @section.heading = outlinee
-    #       @outlinee.outline.append @section 
-    #     else # 4h.3
-    #       puts "4h.3"
-    #       candidate = @section # 4h.3.1
-    #       abort = false
-    #       while !abort
-    #         if rank(outlinee) < rank(candidate.heading) # 4h.3.2
-    #           @section = Section.new
-    #           puts "4h.3.2"
-    #           @section.heading = outlinee
-    #           candidate.append @section
-    #           abort = true
-    #         end
-    #         # TODO: 4h.3.3
-    #         candidate = candidate.parent # 4h.3.4
-    #       end # 4h.3.5
-    #     end
-    #     @stack.push outlinee # 4h.4q
-    #   end
-    # end
-
-    # def exit(outlinee)
-    #   puts "exiting: #{outlinee.node.name}"
-
-    #   if @stack.length > 0 && heading?(@stack.last) # 4a
-    #     @stack.pop if @stack.last == outlinee
-    #     return 
-    #   end
-
-    #   if content_section?(outlinee) && @stack.length > 0 # 4d
-    #     puts "4d"
-    #     @outlinee = @stack.pop # 4d.1
-    #     @section = @outlinee.outline.last_section # 4d.2
-    #     outlinee.outline.sections.each { |s| @section.append s } # 4d.3
-    #   end
-
-    #   if root_section?(outlinee) && @stack.length > 0 # 4e
-    #     puts "4e"
-    #     @outlinee = @stack.pop # 4e.1
-    #     @section = @outlinee.outline # 4e.2
-    #     while section.children? # 4e.3
-    #       @section = @section.last_child # 4e.4
-    #     end
-    #   end
-
-    #   if any_section?(outlinee) && @outlinee == outlinee # 4f.1
-    #     @section = @outlinee.outline.first_section # 4f.2
-    #   end
-    # end
-
-    def rank(outlinee)
-      case outlinee.node.name 
-      when /^H([1-6])$/i
-        - Integer($1)
-      when /^H$/i
-        1
-      else
-        0
-      end
-    end
-
-    def any_section?(outlinee)
-      content_section?(outlinee) || root_section?(outlinee)
-    end
-
-    def content_section?(outlinee)
-      node_matches? outlinee, /^ARTICLE|ASIDE|NAV|SECTION$/i
-    end
-
-    def root_section?(outlinee)
-      node_matches? outlinee, /^BLOCKQUOTE|BODY|DETAILS|FIELDSET|FIGURE|TD$/i
-    end
-
-    def heading?(outlinee)
-      node_matches? outlinee, /^H[1-6]|HGROUP$/i
-    end
-
-    def node_matches?(outlinee, regex)
-      node = outlinee.node
-      node && node.element? && node.node_name =~ regex
     end
   end
 end
--- lib/outliner/outlinee.rb
+++ lib/outliner/outlinee.rb
@@ -6,5 +6,38 @@ module Outliner
     def initialize(node)
       @node = node
     end
+
+    def rank
+      case node.name 
+      when /^H([1-6])$/i
+        - Integer($1)
+      when /^H$/i
+        1
+      else
+        0
+      end
+    end
+
+    def content_or_root?
+      content? || root?
+    end
+
+    def content?
+      matches? /^ARTICLE|ASIDE|NAV|SECTION$/i
+    end
+
+    def root?
+      matches? /^BLOCKQUOTE|BODY|DETAILS|FIELDSET|FIGURE|TD$/i
+    end
+
+    def heading?
+      matches? /^H[1-6]|HGROUP$/i
+    end
+
+    private
+
+    def matches?(regex)
+      node && node.element? && node.node_name =~ regex
+    end
   end
 end
